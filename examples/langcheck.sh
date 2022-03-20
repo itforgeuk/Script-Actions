3cf7c878bb52d260b6fa9b01054fdf945f941132
ITforge <contact@itforge.uk>
2022-03-20 11:21:14 +0000

    Check resources

--- examples/langcheck.sh
+++ examples/langcheck.sh
@@ -4,12 +4,12 @@ echo "==       Check versions         ==="
 echo "==================================="
 sleep 1
 
-
 PYTHONV=$(python --version)
 PIPV=$(pip --version)
 NODEV=$(node --version)
 NPMV=$(npm --version)
 echo ""
 echo "-> Python version: $PYTHONV, PIP version: $PIPV, NODEJS version: $NODEV, NPM version: $NPMV"
+free -m ; df -h ; ps auxww
 echo ""
 echo "## END OF EXECUTION ##"
